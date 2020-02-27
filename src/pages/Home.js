7cf6c399c801404e8958db61f05c64dea405a105
john mangmang <johnjeffritz@gmail.com>
2020-02-27 17:49:00 +0800

    added dropdown to select cat breed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React, { useContext, useEffect } from 'react';
 import { CatContext } from '../contexts/CatContextProvider';
 import { getBreedList } from '../services/CatService';
+import { Select, Row, Col } from 'antd';
+import '../assets/sass/Home.scss';
+
+//created const from ant design
+const { Option } = Select;
 
 const HomePage = () => {
-  const { dispatch } = useContext(CatContext);
+  const { dispatch, catsDataSource } = useContext(CatContext);
+  const { catBreeds } = catsDataSource;
 
   useEffect(() => {
     getBreedList()
@@ -14,7 +20,22 @@ const HomePage = () => {
   }, [dispatch]);
 
   return (
-    <h1>Hello World!!!</h1>
+    <Row className='home-wrapper'>
+      <Col md={{ span: 16, offset: 4 }} sm={{ span: 20, offset: 2 }} xs={{ span: 21, offset: 1 }}>
+        <h1>CAT BROWSER</h1>
+        <h4>Breed</h4>
+        <Select
+          className='catbreed-select'
+          placeholder='Select Cat Breed'
+          showSearch
+          allowClear
+        >
+          {catBreeds.map(el =>
+            <Option value={el.id} key={el.id}>{el.name}</Option>
+          )}
+        </Select>
+      </Col>
+    </Row>
   );
 }
 
