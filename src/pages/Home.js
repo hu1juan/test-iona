020fa6749f3faa323d7e0d5c875d95a3134a083c
john mangmang <johnjeffritz@gmail.com>
2020-02-27 17:22:57 +0800

    intergrate context and reducers to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { CatContext } from '../contexts/CatContextProvider';
+import { getBreedList } from '../services/CatService';
 
 const HomePage = () => {
+  const { dispatch } = useContext(CatContext);
+
+  useEffect(() => {
+    getBreedList()
+      .then(({ data }) => {
+        console.log(data);
+        dispatch({ type: 'GET_BREEDS', data });
+      })
+  }, [dispatch]);
+
   return (
     <h1>Hello World!!!</h1>
   );
