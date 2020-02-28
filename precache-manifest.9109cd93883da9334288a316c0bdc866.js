8bad23e5d0364b78db8290802dd7ddc97c5333ef
john mangmang <johnjeffritz@gmail.com>
2020-02-28 10:12:08 +0800

    Updates

---
+++ precache-manifest.9109cd93883da9334288a316c0bdc866.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63601a7cd86b7c02aa74319cfd298b05",
+    "url": "/hu1juan/test-iona/index.html"
+  },
+  {
+    "revision": "3254fee060a05f26c8f1",
+    "url": "/hu1juan/test-iona/static/css/4.d88ce3aa.chunk.css"
+  },
+  {
+    "revision": "09dd7f1f768c99f9bd6e",
+    "url": "/hu1juan/test-iona/static/css/5.acf49c5e.chunk.css"
+  },
+  {
+    "revision": "dab3ef65d5db5529cc55",
+    "url": "/hu1juan/test-iona/static/css/main.843892a3.chunk.css"
+  },
+  {
+    "revision": "b99150809382d1d40a25",
+    "url": "/hu1juan/test-iona/static/js/0.5583617f.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/hu1juan/test-iona/static/js/0.5583617f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f6e84e8b912635ec51",
+    "url": "/hu1juan/test-iona/static/js/3.79f69bac.chunk.js"
+  },
+  {
+    "revision": "3254fee060a05f26c8f1",
+    "url": "/hu1juan/test-iona/static/js/4.2a292cf8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/hu1juan/test-iona/static/js/4.2a292cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dd7f1f768c99f9bd6e",
+    "url": "/hu1juan/test-iona/static/js/5.7c99d95a.chunk.js"
+  },
+  {
+    "revision": "e1064da56b68393a464a",
+    "url": "/hu1juan/test-iona/static/js/6.37e2d654.chunk.js"
+  },
+  {
+    "revision": "dab3ef65d5db5529cc55",
+    "url": "/hu1juan/test-iona/static/js/main.3da2677c.chunk.js"
+  },
+  {
+    "revision": "9b24b8169247e2582ca9",
+    "url": "/hu1juan/test-iona/static/js/runtime-main.f7053f63.js"
+  }
+]);
